21f027dc2c72d230832ab9b5508c671b92db41e2
tatemouser <tatesmouser@gmail.com>
2024-03-09 13:43:15 -0600

    Version 1.1.0 - Improved mobile UI. Notes: remove comments, add back side bar, style touchups

--- script.js
+++ script.js
@@ -18,3 +18,42 @@ $(document).ready(function () {
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 });
+
+$(window).resize(function() {
+    var recaptcha = $(".h-captcha");
+    if(recaptcha.css('margin') == '1px') {
+        var newScaleFactor = recaptcha.parent().innerWidth() / 304;
+        recaptcha.css('transform', 'scale(' + newScaleFactor + ')');
+        recaptcha.css('transform-origin', '0 0');
+    }
+    else {
+        recaptcha.css('transform', 'scale(1)');
+        recaptcha.css('transform-origin', '0 0');
+    }
+});
+
+function resizeReCaptcha() {
+
+    var width = $( ".h-recaptcha" ).parent().width();
+  
+    if (width < 302) {
+        var scale = width / 302;
+    } else {
+        var scale = 1;
+    }
+  
+    $( ".h-captcha" ).css('transform', 'scale(' + scale + ')');
+    $( ".h-captcha" ).css('-webkit-transform', 'scale(' + scale + ')');
+    $( ".h-captcha" ).css('transform-origin', '0 0');
+    $( ".h-captcha" ).css('-webkit-transform-origin', '0 0');
+  };
+  
+  $( document ).ready(function() {
+  
+      $( window ).on('resize', function() {
+          resizeReCaptcha();
+      });
+  
+      resizeReCaptcha();
+  
+  });
